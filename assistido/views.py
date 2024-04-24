4a368c3c0668d7e909574ffe7d53414026d72c81
Leonardson Silva <leonardsonsilva.dit@gmail.com>
2024-04-24 10:14:50 -0400

    :sparkles: feat: app assistido

--- assistido/views.py
+++ assistido/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.request import HttpRequest
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    return render(
+        request=request,
+        template_name="assistido/lista_assistidos.html",
+        context={}
+    )
